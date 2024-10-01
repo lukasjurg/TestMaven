fa32be090ea1c2d78a362d83ef255ac822f5624e
lukasjurg <lukas.jurgutis@stud.viko.lt>
2024-10-01 15:42:13 +0100

    Add files via upload

---
+++ TestMaven/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] values = new double[] {65, 51 , 16, 11 , 6519, 191 ,0 , 98, 19854, 1, 32};
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+        for (double v : values) {
+            descriptiveStatistics.addValue(v);
+        }
+        double mean = descriptiveStatistics.getMean();
+        double median = descriptiveStatistics.getPercentile(50);
+        double standardDeviation = descriptiveStatistics.getStandardDeviation();
+
+        System.out.println("Mean: " + mean);
+        System.out.println("Median: " + median);
+        System.out.println("Standard Deviation: " + standardDeviation);
+    }
+}
